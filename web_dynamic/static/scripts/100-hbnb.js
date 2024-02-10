26c3f779689ff23315683d821ab0876f6966e0b4
LusterPearl <Salamioluwatosin90@gmail.com>
2024-02-10 12:42:35 -0800

    okay

---
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  const selectedStates = {};
+  const selectedCities = {};
+
+  $('input[type="checkbox"].state-checkbox').change(function () {
+  });
+
+  $('input[type="checkbox"].city-checkbox').change(function () {
+  });
+
+  function updateLocations () {
+  }
+
+  $('button').click(function () {
+  });
+});
